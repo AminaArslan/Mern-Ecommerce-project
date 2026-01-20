23539e3c05ed46137672e87ff36e96b23a2ac183
AminaArslan <aminahameed369@gmail.com>
2026-01-20 11:34:17 +0500

    add

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-   images: {
-    domains: ['res.cloudinary.com'], // <- ye add karo
-   }
 };
 
 export default nextConfig;
